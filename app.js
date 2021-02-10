275b8c1e163c9f76d3186c3ae28e75de6c01de4d
George Gorchev <georgegorchev@yandex.ru>
2021-02-10 11:52:49 +0300

    add: lint

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+// const { errors } = require('celebrate'); <- Включить для проверки ошибок JOI / Celebrate
+// const cors = require('cors');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+// // Милдверы:
+// app.use(cors()); // CORS
+// app.use(bodyParser.urlencoded({ // Парсер
+//   extended: true,
+// }));
+// app.use(bodyParser.json()); // Парсер
+// app.use(requestLogger); // Логгер
+
+// Роутинг:
+app.use('/', (req, res) => {
+  console.log(res);
+});
+
+// Run App:
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
