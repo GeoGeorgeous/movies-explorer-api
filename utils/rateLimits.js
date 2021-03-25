38e82e04ca14128a4aa934d1ca9e6f32dd130933
George Gorchev <georgegorchev@yandex.ru>
2021-03-25 22:55:55 +0300

    fix: limiter

--- utils/rateLimits.js
+++ utils/rateLimits.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 минут
-  max: 100, // лимит на каждый IP адресс — до 100 запросов за windowMs
+  max: 500, // лимит на каждый IP адресс — до 500 запросов за windowMs
 });
 
 module.exports = limiter;
